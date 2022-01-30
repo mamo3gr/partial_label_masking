f6815be8d763a501ba1e7a74389fedfc3a557b88
Mamoru Miura <mamo3gr@gmail.com>
2022-01-30 23:25:35 +0900

    :sparkles: Mask loss

--- examples/mnist/train_model.py
+++ examples/mnist/train_model.py
@@ -5,7 +5,7 @@ import tensorflow as tf
 from tensorflow.keras import Model
 from tensorflow.keras.layers import Conv2D, Dense, Flatten
 
-from examples.utils import set_gpu_memory_growth
+from examples.utils import get_y_true, set_gpu_memory_growth
 
 
 def train_model():
@@ -22,11 +22,15 @@ def train_model():
     test_loss = tf.keras.metrics.Mean(name="test_loss")
     test_accuracy = tf.keras.metrics.BinaryAccuracy(name="test_accuracy")
 
+    positive_ratio = _get_positive_ratio(train_ds).astype(np.float32)
+    ideal_positive_ratio = np.full_like(positive_ratio, 0.5).astype(np.float32)
+
     @tf.function
-    def train_step(images, target_vectors):
+    def train_step(images, target_vectors, mask):
         with tf.GradientTape() as tape:
             predictions = model(images)
             prediction_loss = loss_object(target_vectors, predictions)
+            prediction_loss *= tf.cast(mask, prediction_loss.dtype)
             regularization_loss = tf.math.add_n(model.losses)
             total_loss = prediction_loss + regularization_loss
         gradients = tape.gradient(total_loss, model.trainable_variables)
@@ -46,7 +50,8 @@ def train_model():
     n_epochs = 5
     for epoch in range(n_epochs):
         for images, target_vectors in train_ds:
-            train_step(images, target_vectors)
+            mask = generate_mask(target_vectors, positive_ratio, ideal_positive_ratio)
+            train_step(images, target_vectors, mask)
 
         for test_images, test_target_vectors in test_ds:
             test_step(test_images, test_target_vectors)
@@ -82,6 +87,55 @@ class MyModel(Model):
         return self.d2(x)
 
 
+def generate_mask(y_true, positive_ratio, positive_ratio_ideal):
+    n_samples = y_true.shape[0]
+
+    over_predicted = tf.stack([positive_ratio > positive_ratio_ideal] * n_samples)
+    under_predicted = tf.math.logical_not(over_predicted)
+
+    prob_for_over_predicted = tf.stack(
+        [positive_ratio_ideal / positive_ratio] * n_samples
+    )
+    prob_for_under_predicted = 1.0 / prob_for_over_predicted
+
+    ones_for_over_predicted = multi_hot_with_prob(
+        prob_for_over_predicted, shape=y_true.shape
+    )
+    ones_for_under_predicted = multi_hot_with_prob(
+        prob_for_under_predicted, shape=y_true.shape
+    )
+
+    mask = tf.where((y_true > 0) & over_predicted, ones_for_over_predicted, 1)
+    mask = tf.where((y_true == 0) & under_predicted, ones_for_under_predicted, mask)
+
+    return mask
+
+
+def multi_hot_with_prob(prob, shape):
+    """
+    Generate tensor where some elements are 1 with a certain probability
+    *prob* and the others is 0.
+
+    Args:
+        prob: probability. [0, 1]
+        shape: output shape.
+
+    Returns:
+        ones_with_probability: tensor whose shape is *shape*.
+    """
+    return tf.where(
+        tf.random.uniform(shape=shape, minval=0.0, maxval=1.0) <= prob, 1, 0
+    )
+
+
+def _get_positive_ratio(ds):
+    y = get_y_true(ds)
+    n_samples = len(y)
+    n_positives = np.sum(y > 0, axis=0)
+    positive_ratio = n_positives / n_samples
+    return positive_ratio
+
+
 def _create_train_and_test_datasets() -> Tuple[Any, Any]:
     mnist = tf.keras.datasets.mnist
     (x_train, y_train), (x_test, y_test) = mnist.load_data()
@@ -107,7 +161,7 @@ def _create_train_and_test_datasets() -> Tuple[Any, Any]:
     )
     shuffle_buffer_size = len(train_ds)
     train_ds = train_ds.shuffle(buffer_size=shuffle_buffer_size)
-    batch_size = 1024
+    batch_size = 8192
     train_ds = train_ds.batch(batch_size=batch_size)
 
     test_ds = tf.data.Dataset.zip(
