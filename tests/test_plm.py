848a64e71aa7ee7caaf42ffd8746835b0c38162e
Mamoru Miura <mamo3gr@gmail.com>
2022-02-04 12:16:11 +0900

    :recycle: Implement mask generation as a class

--- tests/test_plm.py
+++ tests/test_plm.py
@@ -1,8 +1,79 @@
+from itertools import combinations
+
 import numpy as np
 from numpy.testing import assert_allclose
 from pytest_mock import MockerFixture
 
-from plm import ProbabilityHistograms, generate_mask, multi_hot_with_prob
+from plm import (
+    MaskGenerator,
+    ProbabilityHistograms,
+    RandomMultiHotGenerator,
+    generate_mask,
+    multi_hot_with_prob,
+)
+
+
+class TestRandomMultiHotGenerator:
+    def test_generate(self):
+        gen = RandomMultiHotGenerator()
+        prob = np.array([[0.1, 0.9, 0.7], [0.2, 0.3, 0.6]])
+        multi_hot = gen.generate(prob)
+        assert multi_hot.shape == prob.shape
+        assert multi_hot.dtype == np.int
+        assert ((multi_hot == 0) | (multi_hot == 1)).all()
+
+    def test_idempotent(self):
+        n_iterations = 5
+        seed = 42
+        prob = np.array([[0.1, 0.9, 0.7], [0.2, 0.3, 0.6]])
+
+        multi_hots = []
+        for _ in range(n_iterations):
+            gen = RandomMultiHotGenerator(seed=seed)
+            multi_hot = gen.generate(prob)
+            multi_hots.append(multi_hot)
+
+        for a, b in combinations(multi_hots, r=2):
+            assert_allclose(a, b)
+
+
+class FixedMultiHotGenerator(RandomMultiHotGenerator):
+    def __init__(self):
+        super(FixedMultiHotGenerator, self).__init__()
+
+    def generate(self, prob: np.ndarray) -> np.ndarray:
+        return np.where(prob >= 0.8, 1, 0)
+
+
+class TestMaskGenerator:
+    def test_generate_mask(self, mocker: MockerFixture):
+        # n_classes = 3
+        # n_samples = 2
+
+        # class[0] is head (frequent)
+        # class[1] is tail (infrequent)
+        # class[2] is medium
+        positive_ratio = np.array([0.8, 0.1, 0.5])
+        positive_ratio_ideal = np.array([0.6, 0.4, 0.5])
+        # ideal / real = [0.75, 4,    1]
+        # real / ideal = [1.33, 0.25, 1]
+
+        # fmt: off
+        y_true = np.array([
+            [1, 0, 1],
+            [0, 1, 0],
+        ])
+        # loss for head class's positive and tail class's negative should be masked
+        # (multiplied by zero)
+        mask_expect = np.array([
+            [0, 0, 1],
+            [1, 1, 1],
+        ])
+        # fmt: on
+
+        gen = MaskGenerator(generator=FixedMultiHotGenerator())
+        mask_actual = gen.generate(y_true, positive_ratio, positive_ratio_ideal)
+        assert_allclose(mask_actual, mask_expect)
 
 
 def test_generate_mask(mocker: MockerFixture):
